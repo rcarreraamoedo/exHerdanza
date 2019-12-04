09e83e69c230e92b5f80184e034d2160c3828b64
rcarreraamoedo <rcarreraamoedo@danielcastelao.org>
2019-12-04 10:39:14 +0100

    Cambios Completado

--- src/exherdanza/ExHerdanza.java
+++ src/exherdanza/ExHerdanza.java
@@ -10,8 +10,22 @@ public class ExHerdanza {
         System.out.println(sc.toString());
         System.out.println(sc);// == anterior
         
-        Libro obx = new Libro("Pepe","ISBN","Titulo",50,4.5f,"nome");
+        Libro obx = new Libro("Pepe,","1234","tut",7854,83.6f);
         System.out.println(obx.toString());
+        
+        //Polimorfismo da subclase Libro a Pulicacion
+        Publicacion lib = new Libro ("bbb","1111","titulo",200,3.5f);
+        System.out.println(lib.toString());
+        
+        //Polimorfismo da subclase Revista a Publicacion
+        Publicacion revis = new Revista(750,15.5f,"revista");
+        System.out.println(revis.toString());
+        
+        //Outro método de polimorfismo
+        Publicacion lib2 = new Libro();
+        Publicacion revis2 = new Revista();
+        lib2.amosar();
+        revis2.amosar();
     }
     
 }
